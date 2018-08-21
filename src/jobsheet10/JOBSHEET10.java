d44b67131dff3bbb85ccd09e16361c8d59f5b5b8
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-21 15:09:38 +0700

    Initial Commit

---
+++ src/jobsheet10/JOBSHEET10.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet10;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class JOBSHEET10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        //menbuat array
+        String [] nama = {"Linda", "Santi", "Susan", "Mila", "Ayu"};
+        //mengambil data string
+        
+        System.out.println(nama[2]);
+        
+    }
+    }
+    
+
